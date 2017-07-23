ec72a0a180047dfbbdbf41774a160b653e3d7d8d
xiaogang00 <872310734@qq.com>
2017-07-23 22:22:12 +0800

    code and note for UNIX

---
+++ code-chapter8/chapter8.c
@@ -0,0 +1,17 @@
+# include <sys/types.h>
+# include <unistd.h>
+
+pid_t getpid(void);
+pid_t getppid(void);
+uid_t getuid(void);
+uid_t geteuid(void);
+gid_t getgid(void);
+gid_t getegid(void);
+
+
+pid_t fork(void);
+
+pid_t wait3(int *statloc, int options, struct rusage *rusage);
+pid_t wait4(pid_t pid, int *statloc, int options, struct rusage *rusage);
+
+
