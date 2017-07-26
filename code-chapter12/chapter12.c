9a0be5c6401c2b3383eb47a9f5aceaed5192e4a4
xiaogang00 <872310734@qq.com>
2017-07-26 20:45:10 +0800

    code and note for UNIX

--- code-chapter12/chapter12.c
+++ code-chapter12/chapter12.c
@@ -52,3 +52,32 @@ static void lockabyte(const char *name, int fd, off_t offset)
     printf("%s: got the lock, byte %d \n", name, offset);
 }
 
+//打印流中的模块名
+#include <sys/conf.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stropts.h>
+#include "ourhdr.h"
+
+int main(int argc, char * argv[])
+{
+    int fd,i,nmods;
+    struct str_list list;
+    if(argc != 2)
+       err_quit("usage: a.out <pathname>");
+    if((fd =open(argv[1], O_RDONLY)) < 0)
+       err_sys("can't open %s", argv[1]);
+    if(isastream(fd) == 0)
+       err_quit("%s is not a stream", argv[1]);
+    if((nmods = ioctl(fd, I_LIST,(void *) 0)) < 0)
+       err_sys("I_LIST error for nmods");
+    printf("modules = %d\n", nmods);
+    list.sl_modlist = calloc(nmods, sizeof(struct str_mlish));
+    if(list.sl_modlist == NULL)
+       err_sys("calloc error");
+    list.sl_nmods = nmods;
+    if(ioctl(fd, I_LIST, &list) < 0)
+       err_sys("I_LIST error for list");
+    for(i = 1; i <= nmods; i++)
+       printf("%s : %s\n", (i==nmods)?"driver":"module", list.sl_modlist++);
+}
